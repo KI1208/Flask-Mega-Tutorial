de11aab66643bf0d06096fdfed6599fe2e98e616
ikedak2 <Keisuke.Ikeda@emc.com>
2019-03-29 10:45:54 +0900

    Chap5. User Loader

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+
+from app import routes, models
+
+# bottom import is a workaround to circular import, a common problem with Flask application
+
